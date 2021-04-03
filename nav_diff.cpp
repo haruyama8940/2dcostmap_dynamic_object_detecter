63a215c98230b1b6e7927ae71358d42ae1ebc092
Tatsuhiro Ikebe <40545422+uhobeike@users.noreply.github.com>
2021-04-03 22:58:51 +0900

    Update nav_diff.cpp

--- nav_diff.cpp
+++ nav_diff.cpp
@@ -1,19 +1,20 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
-#include <nav_msgs/MapMetaData.h>
+
 #include <vector>
 #include <numeric>
 
-using std::
-void nav_callback(msg)
+using namespace::std;
+
+void nav_callback(const nav_msgs::OccupancyGrid& msg)
 {//callback関数
     
     int sum_new, sum_old;//新旧合計
     //float avg_new, avg_old;
-    std::vector<int>nav_new{msg.data};
-    std::vector<int>nav_old;
+    vector<int> nav_new{msg.data};
+    vector<int> nav_old;
 
-    for(int n = 0; n < nav_new.size(); n++){//ひとつの差
+    for (int n = 0; n < nav_new.size(); n++){//ひとつの差
         auto diff+ = nav_new[n]-nav_old[n];
 
     }
