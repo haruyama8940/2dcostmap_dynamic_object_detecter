fdddff4d3442bfccba4495683ceb11466559505a
haruyama8940 <yaruo0406@gmail.com>
2021-04-05 21:56:48 +0900

    vector

--- nav_diff.cpp
+++ nav_diff.cpp
@@ -1,37 +1,63 @@
 #include <ros/ros.h>
 #include <nav_msgs/OccupancyGrid.h>
+#include <map_msgs/OccupancyGridUpdate.h>
 
 #include <vector>
 #include <numeric>
 
 using namespace::std;
 
-void nav_callback(const nav_msgs::OccupancyGrid& msg)
+void nav_callback(const nav_msgs::OccupancyGridUpdateConstPtr& msg)
 {//callback関数
     bool nav_flag=true;
-    
-    //float avg_new, avg_old;
-    vector<int8_t> nav_new;
-    vector<int8_t> nav_old;
-    
+    int i,n = 0;
+    vector<int> nav_new;
+    vector<int> nav_old;
+    cout << msg->data.size() << "\n";
+
     if (nav_flag){
-        nav_old = msg.data;
+        for (i=0;i<msg->data.size();++i){
+
+            if (msg->data[i] == NULL){
+                cout << "0,";
+                nav_old[i] = 0;
+            }   
+            if (msg->data[i] == 'd'){
+                cout << "100,";
+                nav_old[i] = 100;
+            }
+
+            if (msg->data[i] == 'c'){
+                cout << "99,";
+                nav_old[i] = 99;
+            }
+        }
         nav_flag = false;
     }
+
     else if (!nav_flag){
-        nav_new = msg.data;
+        for (i=0;i<msg->data.size();++i){
+
+            if (msg->data[i] == NULL){
+                //cout << "0,";
+                nav_new[i] = 0;
+            }
+             if (msg->data[i] == 'c'){
+                //cout << "99,";
+                nav_new[i] = 99;
+            }
+            if (msg->data[i] == 'd'){
+                //cout << "100,";
+                nav_new[i] = 100;
+            }
+        }
         for (int n = 0; n <= nav_new.size(); ++n){//ひとつの差
             diff += abs(nav_new[n]-nav_old[n]);
         }
         nav_old = nav_new;//内容の代入vectorならできるらしい
         cout << "diff" << "\n";
     }
-    /*平均
-    avg_new = std::accumulate(nav_new.begin(),nav_new.end(), 0); 
-    auto diff= avg_new - avg_old
-    std::cout << "sum_new = "<<std::accumulate(nav_new.begin(),nav_new.end() 0) << "\n";
-    std::cout << "avg= "<<std::accumulate(nav_new.begin(),nav_new.end() 0) << "\n";
-    */
+    
 }
 
 
@@ -39,15 +65,10 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "nav_diff");
     ros::NodeHandle nh;
-    ros::Subscriber sub = nh.Subscribe("/move_base/local_costmap/costmap", 1, nav_callback);
-    
-    bool TwoLoopCnt = 0;
+    ros::Subscriber sub = nh.subscribe("/move_base/local_costmap/costmap_updates", 100, nav_callback);
 
     ros::Rate loop_rate(1);
     while (ros::ok){
-        if (TwoLoopCnt == 2)
-            break;
-        TwoLoopCnt++;
         ros::spinOnce();
         loop_rate.sleep();
     }
