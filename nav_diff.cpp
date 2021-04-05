8258894d7e063efd7367d772ff8ff0e5e1b95579
haruyama8940 <yaruo0406@gmail.com>
2021-04-05 21:58:34 +0900

    cnt++

--- nav_diff.cpp
+++ nav_diff.cpp
@@ -9,28 +9,19 @@ using namespace::std;
 
 void nav_callback(const nav_msgs::OccupancyGridUpdateConstPtr& msg)
 {//callback関数
+
     bool nav_flag=true;
-    int i,n = 0;
-    vector<int> nav_new;
-    vector<int> nav_old;
+    int i, n, cnt_old, cnt_new = 0;
+
     cout << msg->data.size() << "\n";
 
     if (nav_flag){
         for (i=0;i<msg->data.size();++i){
 
-            if (msg->data[i] == NULL){
-                cout << "0,";
-                nav_old[i] = 0;
+            if (msg->data[i]!== NULL){
+                cnt_old++;
+                //nav_old[i] = 0;
             }   
-            if (msg->data[i] == 'd'){
-                cout << "100,";
-                nav_old[i] = 100;
-            }
-
-            if (msg->data[i] == 'c'){
-                cout << "99,";
-                nav_old[i] = 99;
-            }
         }
         nav_flag = false;
     }
@@ -38,23 +29,14 @@ void nav_callback(const nav_msgs::OccupancyGridUpdateConstPtr& msg)
     else if (!nav_flag){
         for (i=0;i<msg->data.size();++i){
 
-            if (msg->data[i] == NULL){
+            if (msg->data[i]!== NULL){
                 //cout << "0,";
-                nav_new[i] = 0;
-            }
-             if (msg->data[i] == 'c'){
-                //cout << "99,";
-                nav_new[i] = 99;
-            }
-            if (msg->data[i] == 'd'){
-                //cout << "100,";
-                nav_new[i] = 100;
+                //nav_new[i] = 0;
+                cnt_new++;
             }
         }
-        for (int n = 0; n <= nav_new.size(); ++n){//ひとつの差
-            diff += abs(nav_new[n]-nav_old[n]);
-        }
-        nav_old = nav_new;//内容の代入vectorならできるらしい
+
+        diff = cnt_old - cnt_new;
         cout << "diff" << "\n";
     }
     
