2c9b4f831dd03d654216cc22b08154363270140a
Tatsuhiro Ikebe <40545422+uhobeike@users.noreply.github.com>
2021-04-07 23:16:44 +0900

    Update 2dcostmap_dynamic_object_detecter.cpp

--- 2dcostmap_dynamic_object_detecter/src/2dcostmap_dynamic_object_detecter.cpp
+++ 2dcostmap_dynamic_object_detecter/src/2dcostmap_dynamic_object_detecter.cpp
@@ -4,56 +4,43 @@
 
 using namespace::std;
 
-void nav_callback(const map_msgs::OccupancyGridUpdateConstPtr& msg)
-{//callback関数
-
-    static bool nav_flag=true;
-    int i, n = 0;
+void CostMapDiffCb(const map_msgs::OccupancyGridUpdateConstPtr& msg)
+{
+    static bool first_flag=true;
     static int cnt_old, cnt_new = 0;
+    static uint diff = 0, diff_old = 0, diff_new = 0;
 
-    // cout << msg->data.size() << "\n";
-
-    if (nav_flag){
-        for (i=0;i<msg->data.size();++i){
-
-            if (msg->data[i] == '\0'){
+    if (first_flag){
+        for (int i=0;i<msg->data.size();++i){
+            if (msg->data[i] == '\0')
                 cnt_old++;
-                //nav_old[i] = 0;
-            }   
         }
-        nav_flag = false;
+        first_flag = false;
     }
-
-    else if (!nav_flag){
-        for (i=0;i<msg->data.size();++i){
-
-            if (msg->data[i] == '\0'){
-                //cout << "0,";
-                //nav_new[i] = 0;
+    else if (!first_flag){
+        for (int i=0;i<msg->data.size();++i){
+            if (msg->data[i] == '\0')
                 cnt_new++;
-            }
         }
-
-        int diff = cnt_new - cnt_old;
+        diff_new = abs(cnt_new - cnt_old);
+        diff = diff_new + diff_old;
+        diff_old = diff_new;
         cnt_old = cnt_new;
         cnt_new = 0;
         cout << diff << "\n";
     }
-    
 }
-
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "nav_diff");
+    ros::init(argc, argv, "costmap_dynamic_object_detecter");
     ros::NodeHandle nh;
-    ros::Subscriber sub = nh.subscribe("/move_base/local_costmap/costmap_updates", 100, nav_callback);
+    ros::Subscriber sub = nh.subscribe("/move_base/local_costmap/costmap_updates", 1, CostMapDiffCb);
 
-    ros::Rate loop_rate(1);
+    ros::Rate loop_rate(0.3);
     while (ros::ok){
         ros::spinOnce();
         loop_rate.sleep();
-    }
-    
+    }    
     return 0;
 }
